93e303224b1c03e9c4dc3d90d2d5291bca648c56
Torkel Ödegaard <torkel.odegaard@gmail.com>
2014-12-02 14:42:18 -0800

    Site updated to decc4ab

--- assets/bower_components/jquery/src/css/swap.js
+++ assets/bower_components/jquery/src/css/swap.js
@@ -1,1 +1,1 @@
-define(["../core"],function(e){return e.swap=function(e,t,n,i){var r,o,s={};for(o in t)s[o]=e.style[o],e.style[o]=t[o];r=n.apply(e,i||[]);for(o in t)e.style[o]=s[o];return r},e.swap});
+define(["../core"],function(t){return t.swap=function(t,e,n,i){var s,a,r={};for(a in e)r[a]=t.style[a],t.style[a]=e[a];s=n.apply(t,i||[]);for(a in e)t.style[a]=r[a];return s},t.swap});
