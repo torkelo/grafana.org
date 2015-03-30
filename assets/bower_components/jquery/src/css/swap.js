75fa65639e55277327fb40fa76d1e6e9c10e31d5
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-03-30 16:24:00 +0200

    Site updated to 4ef1c03

--- assets/bower_components/jquery/src/css/swap.js
+++ assets/bower_components/jquery/src/css/swap.js
@@ -1,1 +1,1 @@
-define(["../core"],function(e){return e.swap=function(e,t,n,i){var o,r,a={};for(r in t)a[r]=e.style[r],e.style[r]=t[r];o=n.apply(e,i||[]);for(r in t)e.style[r]=a[r];return o},e.swap});
+define(["../core"],function(e){return e.swap=function(e,t,n,i){var r,o,a={};for(o in t)a[o]=e.style[o],e.style[o]=t[o];r=n.apply(e,i||[]);for(o in t)e.style[o]=a[o];return r},e.swap});
